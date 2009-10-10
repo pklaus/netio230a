21ae7f5fe26091c7173e69c6d47c3efdfe01f7bc
pklaus <philipp.klaus@gmail.com>
2009-10-10 21:51:30 +0200

    changes to PyS60 interface

--- example.PyS60.py
+++ example.PyS60.py
@@ -40,25 +40,6 @@ import e32
 host = "your.dyndns.org"
 port = 23
 pw = "your chosen password"
- 
-
-def exit_key_handler():
-    app_lock.signal()
-
-def switchPort():
-    portToChange,portOn = appuifw.multi_query(u"Port to switch:",u"new status:")
-    if portToChange == None or portOn == None:
-        return
-    netio.setPortPower(int(portToChange),bool(int(portOn)))
-    updateStatus()
-
-#def subitem1():
-#    messagecanvas.set(u'Now first subitem was selected')
-
-def updateStatus():
-    portPower = netio.getPortList()
-    messagecanvas.set( u"power status:\nport 1: %s\nport 2: %s\nport 3: %s\nport 4: %s" % (portPower[0],portPower[1],portPower[2],portPower[3] ) )
-
 
 appuifw.app.title = u"NETIO 230A"
 appuifw.note(u"Welcome to the Koukaam NETIO 230A control.", 'info')
@@ -68,12 +49,45 @@ messagecanvas = appuifw.Text()
 
 try:
     netio = netio230a.netio230a(host, "admin", pw, True, port)
-except StandardError:
+except:
+    netio=None
     messagecanvas.set(u"could not connect")
 
+
+def exit_key_handler():
+    app_lock.signal()
+
+def switchPort():
+    global netio
+    portToChange = appuifw.query(u"Port to switch:", 'number')
+    portOn = appuifw.query(u"new status for port %s:"% portToChange, 'number')
+    if portToChange == None or portOn == None:
+        return
+    netio.setPortPower(int(portToChange),bool(int(portOn)))
+    updateStatus()
+
+#def subitem1():
+#    messagecanvas.set(u'Now first subitem was selected')
+
+def updateStatus():
+    global netio
+    portPower = netio.getPortList()
+    messagecanvas.set( u"" )
+    messagecanvas.style = appuifw.STYLE_BOLD
+    messagecanvas.add(u"Power Status:\n\n")
+    messagecanvas.style = 0
+    messagecanvas.add(u"port 1: %s\nport 2: %s\nport 3: %s\nport 4: %s" % (portPower[0],portPower[1],portPower[2],portPower[3] ))
+
 def main():
+    global netio
+    
     appuifw.app.screen='large'
     appuifw.app.body = messagecanvas
+    
+    if netio == None:
+        # wait 3 seconds:
+        e32.ao_sleep(3)
+        appuifw.app.set_exit() # this completely closes python
     
     #appuifw.app.menu = [(u"Submenu 1", ((u"sub item 1", subitem1), (u"sub item 2", subitem2))), (u"Exit", exit_key_handler)]
     appuifw.app.menu = [(u"Switch Port", switchPort), (u"Refresh Status", updateStatus), (u"Exit", exit_key_handler)]
@@ -82,6 +96,7 @@ def main():
     appuifw.app.exit_key_handler = exit_key_handler
 
     app_lock.wait()
+    netio = None
     
  
 if __name__ == '__main__':
